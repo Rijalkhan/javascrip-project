f6dec4dc6d4985a4062847c2f66ca1a2dbf32f54
Rijalkhan <64781808+Rijalkhan@users.noreply.github.com>
2020-06-15 07:44:28 -0700

    javascrip assignment

---
+++ project javascript/app.js
@@ -0,0 +1,116 @@
+//==== Chapter # 1 Task # 1 ====//
+
+// alert("Hello World");
+
+
+
+//==== Chapter # 1 Task # 2 ====//
+ 
+ // alert("Error! Please enter a valid password.");
+
+  
+ 
+ 
+ //==== Chapter # 1 Task # 3 ====//
+
+  //  alert( "welcome to js land ...\n Happy Coding");
+
+
+  
+ //==== Chapter # 1 Task # 4 ====//
+ 
+ // alert( "welcome to js land ");
+ // alert( "Happy Coding \n Prevent this page from creating additional dialougs ");
+
+
+ //==== Chapter # 1 Task # 5 ====//
+ 
+ // console.log('Hello ... i can run js through my web browsers console'); 
+
+
+ //==== Chapter # 2 Task # 1 ====//
+
+ // var Username = 'Rijal';
+ // alert(Username);
+
+
+ //==== Chapter # 2 Task # 2 ====//
+
+ //var myName  = 'Muhammad Rijal Khan';
+  //alert(myName);
+
+
+  //==== Chapter # 2 Task # 3 ====//
+
+ //var Message  = 'Hello World';
+  //alert(Message);
+
+
+   //==== Chapter # 2 Task # 4 ====//
+
+//  var myName  = 'Rijalkhan' ;
+//   alert(myName);
+//   var age  = '18' ;
+//   alert(age);
+//   var Message  = 'Certificate Mobile app Development' ;
+//   alert(Message);
+  
+ //==== Chapter # 2 Task # 5 ====//
+
+//  alert("Pizza \nPizz \nPiz \nPi \nP");
+
+
+//==== Chapter # 2 Task # 6 ====//
+
+// var email = 'my email address is rijalkhan25@gmail.com';
+// alert(email);
+
+
+//==== Chapter # 2 Task # 7 ====//
+
+//  var book = 'i am trying to learn from the book a\nsmarter way to learn javascrip';
+//  alert(book);
+
+
+//==== Chapter # 2 Task # 8 ====//
+
+// document.write('<h2> Yah .. I can write html content through javascrip </h2>')
+
+
+//==== Chapter # 2 Task # 9 ====//
+
+// alert('▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬')
+
+
+//==== Chapter # 3 Task # 1 ====//
+
+// prompt('Enter your age')
+// alert ("i am 18 year old")
+
+
+//==== Chapter # 3 Task # 2 ====//
+
+// var visits = GetCookie("counter");
+
+// if (!visits) { visits = 1;
+
+// document.write("By the way, this is your first time here.");
+
+// }
+
+// else {
+
+// visits = parseInt(visits) + 1;
+
+// document.write("I note, you have been here " + visits + " times.");}
+
+
+
+//==== Chapter # 3 Task # 3 ====//
+
+// document.write('<p> my birthday year 2001 <br> data type of my declecrd variable is number </p>')
+
+
+//==== Chapter # 3 Task # 4 ====//
+
+// document.write('<b> Rijalkhan </b> ordered <b> 5 T-Shirt(s) </b> on xyz clothing store ')
